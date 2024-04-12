c04e28c42e9e02ee148fa0b3df3f77c51969e083
cuong457 <pnccuong20@clc.fitus.edu.vn>
2024-04-12 23:36:00 +0700

    ADD: draft PayOs payment

---
+++ DeToiServer/DeToiServer/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using DeToiServer.Services.AccountService;
+using DeToiServer.Services.CustomerAccountService;
+using DeToiServer.Services.FreelanceAccountService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DeToiServer.Controllers
+{
+    [Route("api/v1/admin")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly UnitOfWork _uow;
+        private readonly IMapper _mapper;
+        private readonly IAccountService _accService;
+
+        public AdminController(
+            UnitOfWork uow, 
+            IMapper mapper, 
+            IAccountService accService
+        )
+        {
+            _uow = uow;
+            _mapper = mapper;
+            _accService = accService;
+        }
+
+        // Doanh thu = 5% Tiền dịch vụ
+        // Tổng tiền = Tiền dịch vụ + Doanh thu
+
+    }
+}
---
+++ DeToiServer/DeToiServerCore/Models/Report/ReportStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace DeToiServerCore.Models.Report
+{
+    public class ReportStatus : ModelBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Report>? Reports { get; set; }
+    }
+}
