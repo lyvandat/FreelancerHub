e758c8bc8a0216a522f0ba92eed8f71fab141c7b
cuong457 <pnccuong20@clc.fitus.edu.vn>
2024-04-27 02:17:32 +0700

    UPDATE: Quick fix newest Migration

---
+++ DeToiServer/DeToiServerData/Migrations/20240426191448_V1.6.18.5.1_UpdateServiceType.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeToiServerData.Migrations
+{
+    /// <inheritdoc />
+    public partial class V161851_UpdateServiceType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Keys",
+                table: "ServiceTypes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Keys",
+                table: "ServiceTypes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValue: "");
+        }
+    }
+}
