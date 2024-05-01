225ebe4006620c1cc626aa464dbb37769d319f6e
lyvandat <75932518+lyvandat@users.noreply.github.com>
2024-05-01 16:41:40 +0700

    FIX: Real time connection string

--- DeToiServer/DeToiServer/RealTime/RealtimeConsumer.cs
+++ DeToiServer/DeToiServer/RealTime/RealtimeConsumer.cs
@@ -90,7 +90,7 @@ namespace DeToiServer.RealTime
         {
             // build deployment: _configuration["RealTimeHub"] ?? Helper.GetDockerHostUrl()
             _connectionSignalR = new HubConnectionBuilder()
-               .WithUrl("e")
+               .WithUrl(Helper.GetDockerHostUrl())
                .Build();
             await _connectionSignalR.StartAsync();
         }
