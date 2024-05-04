6997074d7e33b550b2eccebe7c5480004baab5d0
lyvandat <75932518+lyvandat@users.noreply.github.com>
2024-05-04 11:54:29 +0700

    ADD: More CORS

--- DeToiServer/DeToiServer/Program.cs
+++ DeToiServer/DeToiServer/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddJwtBearerAuthentication(builder.Configuration);
 builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins",
     policy =>
     {
-        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        policy.AllowAnyOrigin().AllowAnyMethod().SetIsOriginAllowed(host => true).AllowAnyHeader().AllowCredentials();
         policy.WithOrigins("http://localhost:2014").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
         policy.WithOrigins("http://localhost:2016").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
         policy.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
